0ccf378b29981359b1b61828a25d530124e1abf5
DINESH N <78141949+DINESHN27@users.noreply.github.com>
2022-02-26 21:45:42 +0530

    Add files via upload

---
+++ Counting Characters.py
@@ -0,0 +1,22 @@
+#Counting the character present in the Given string
+
+str1 = str(input("Enter the String : "))
+d = dict()
+for i in str1:
+    d[i] = str1.count(i)
+for i in range(len(d)):
+    max1 = max(d,key=d.get)
+    max2 = max(d.values())
+    print("{} = {}".format(max1,max2))
+    d.pop(max1)
+    
+
+
+
+
+
+
+
+
+
+
